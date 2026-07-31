b3db0d341bfd0c3db62ae8ea92216865dcc60c84
akashnegi62 <akashsing215@gmail.com>
2026-07-31 10:45:08 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function blog() {
   return (
-    <div className='h-screen w-full bg-black flex justify-center items-center text-white text-[5vw]'>Hello Blog 1</div>
+    <div className='h-screen w-full bg-black flex justify-center items-center text-white text-[5vw]'>Hello Blog1</div>
   )
 }
 
